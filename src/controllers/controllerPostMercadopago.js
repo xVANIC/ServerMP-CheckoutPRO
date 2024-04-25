6f2a1dd187f84a5d12cb7fba170a30a3bef09919
Angel Fabian HC <angelfabianhc@gmail.com>
2024-04-25 11:33:14 -0500

    Change in Preference

--- src/controllers/controllerPostMercadopago.js
+++ src/controllers/controllerPostMercadopago.js
@@ -6,7 +6,7 @@ const controllerPostMercadopago = async ({ cart, userData }) => {
     description: item.description || "",
     picture_url: item.picture_url,
     currency_id: "PEN",
-    quantity: item.quantity /*  Number(item.count), */,
+    quantity: item.quantity,
     unit_price: Number(item.unit_price),
   }));
   const response = await mercadopago.preferences.create({
@@ -17,13 +17,21 @@ const controllerPostMercadopago = async ({ cart, userData }) => {
       email: userData.email,
     },
     back_urls: {
-      // success: "https://cine-go-ten.vercel.app/payment_success",
-      // failure: "https://cine-go-ten.vercel.app/payment_failure",
       success: "http://localhost:3000/success",
       failure: "http://localhost:3000/failed",
       pending: "",
     },
     auto_return: "approved",
+    payment_methods: {
+      excluded_payment_methods: [
+        { id: "pagoefectivo_atm" },
+        { id: "bancaInternet" },
+      ],
+      excluded_payment_types: [
+        { id: "ticket" },
+      ],
+      installments: 1,
+    },
   });
 
   if (!response) throw new Error("Error al crear orden");
